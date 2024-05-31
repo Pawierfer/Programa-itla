00f32bb30dda71256db32c389002620dc3c6629f
Pawierfer <william.d.abreu@gmail.com>
2024-05-30 20:15:46 -0400

    Segundo commit

---
+++ capitulo 2/main1.c
@@ -0,0 +1,1 @@
+#include <stdio.h>int main (void) {    float PRO;    printf("Ingrese el promedio del alumno: ");    scanf("%f", &PRO);    if (PRO >= 6)        printf("\nAprobado\n");    return 0;
